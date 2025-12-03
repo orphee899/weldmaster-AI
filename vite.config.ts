538679e6cb1031136d257698b43856905f2f609f
orphee899 <moi@mon-app.com>
2025-12-03 11:26:45 +0100

    Fix scroll mobile

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/weldmaster-AI/',
+export default defineConfig(({ mode }) => {
+  // Charge les variables d'environnement depuis .env.local
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [react()],
+    base: '/weldmaster-AI/',
+    define: {
+      // Cette ligne est MAGIQUE : elle remplace "process.env.API_KEY" par ta vraie clé lors de la construction
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    }
+  }
 })
